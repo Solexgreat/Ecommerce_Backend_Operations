92ec531bca67e1372d199e87e36fd134ea1b846c
Solexgreat <solomonGodkid@gmail.com>
2024-09-22 21:47:21 +0100

    ecommerce_ backend_operations

---
+++ routes/review.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const router = express.Router;
+const {createReview} = require('../controller/reviewController')
+const {updateReview} = require('../controller/reviewController')
